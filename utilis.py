4dee2850b4982dd40b8b871ccbaa1c6c3501c594
mweruli <mwerulijohnmoh@gmail.com>
2023-03-05 23:11:38 +0300

    add_middleware

--- utilis.py
+++ utilis.py
@@ -65,3 +65,11 @@ from fastapi import APIRouter, FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 from scheduler import app as app_rocketry
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
